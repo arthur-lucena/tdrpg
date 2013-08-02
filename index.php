3cee15999c8c0b5f481a623b2944cc5a42def0a2
Arthur Vieira Moreira Lucena <arthurvmx5@gmail.com>
2013-08-02 10:49:09 -0300

    [TDRPG]
    New struct for index page, better html organization, css and js

--- index.php
+++ index.php
@@ -1,64 +1,4 @@
-﻿<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-
-<?php
-define('INCLUDE_CHECK',true);
-
-include 'lib/Functions.php';
-include 'lib/Connect.php';
-include 'class/Board.php';
-include 'class/RolledDice.php';
-include 'class/Member.php';
-include 'control/BoardControl.php';
-include 'control/RolledDiceControl.php';
-include 'control/MemberControl.php';
-
-// ALTER TABLE  `tb_dice_rolled` ADD INDEX (  `id` )
-
-session_name('tdRpg');
-session_set_cookie_params(2*7*24*60*60);
-session_start();
-
-if (isset($_SESSION['id'])) {
-	echo 'usuario logado';
-} else {
-	echo 'usuario nao logado';
-}
-
-echo '<br /><br />';
-
-function _main() {	
-	if (isset($_GET['createboard'])) {
-		if (isset($_SESSION['id'])) {
-			$board = createBoard();
-
-			header('Location:?playing=true&id='.$board->getId().'&host_user='.$board->getHostUser().'&name='.$board->getName());
-		} else {
-			echo 'usuario n�o logado, favor <a href="'.baseURL().'?createacc=true">criar uma conta</a> ou <a href="'.baseURL().'?login=true">logar</a>!';
-		}
-	} else if (isset($_GET['playing'])) {
-		echo 'Id da mesa: '.$_GET['id'].'<br />Host da mesa: '.$_GET['host_user'];
-		
-		include 'pages/board.php';
-	} else if (isset($_GET['seeboard'])) {		
-		include 'pages/seeboard.php';
-	} else if (isset($_GET['createacc'])) {
-		include 'pages/createacc.php';
-	} else if (isset($_GET['login'])) {
-		include 'pages/login.php';
-	} else if (isset($_GET['logout'])) {
-		$_SESSION = array();
-		session_destroy();
-		
-		header('Location: http://'.$_SERVER['SERVER_NAME'].'/tdrpg/');
-		exit;
-	} else {
-		include 'pages/menu.php';
-	}
-}
-
-_main();
-?>
-<!DOCTYPE >
+﻿<!DOCTYPE >
 <html>
 <head>
     <title>TDRPG</title>
@@ -66,41 +6,51 @@ _main();
     <link rel="stylesheet" type="text/css" href="fonts/Bariol-Regular/font.css">
     
     <style>
-    body {
-        background-color:#EEE;
-        /*font-size:62.5%;*/
-    }
-    
-    body, input[type=text], input[type=submit], input[type=reset], #content {
-        font-family:'Bariol-Regular';
-        color:#666;
-        outline: none;
-        font-smoothing:antialiased;
-        -moz-font-smoothing:antialiased;
-        -webkit-font-smoothing:antialiased;
-        -o-font-smoothing:antialiased;
-    }
-    /*
-    label {
-        font-size: 1.2em;   
-    }
-    */
-    input[type=text] {
-        width: 260;
-    }
-    /*
-    input[type=text], #submit, #reset, #content {
-        font-size: 1.6em;
-    }
-    */
+        body {
+            background-color:#EEE;
+            font-size:62.5%;
+        }
+        
+        body, input[type=text], input[type=submit], input[type=reset], #content {
+            font-family:'Bariol-Regular';
+            color:#666;
+            outline: none;
+            font-smoothing:antialiased;
+            -moz-font-smoothing:antialiased;
+            -webkit-font-smoothing:antialiased;
+            -o-font-smoothing:antialiased;
+        }
+        
+        /*label {
+            font-size:1.6em;   
+        }*/
+        
+        input[type=text] {
+            width: 260;
+        }
+        
+        /*input[type=text] {
+            font-size:1.6em;
+        }*/
+        
+        #section {
+            font-size:1.6em;
+        }
     </style>
 </head>
 
 <body>
+<section id="section">
+<?php
+    include '/pages/index.php';
+    _main();
+?>
 <br />
 <br />
 <a rel="license" target="_blank" href="http://creativecommons.org/licenses/by/3.0/deed.pt_BR">
     <img alt="Licença Creative Commons" style="border-width:0" src="http://i.creativecommons.org/l/by/3.0/88x31.png" />
 </a>
+</section>    
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 </body>    
 </html>
---
+++ pages/index.php
@@ -0,0 +1,56 @@
+<?php
+define('INCLUDE_CHECK',true);
+
+include 'lib/Functions.php';
+include 'lib/Connect.php';
+include 'class/Board.php';
+include 'class/RolledDice.php';
+include 'class/Member.php';
+include 'control/BoardControl.php';
+include 'control/RolledDiceControl.php';
+include 'control/MemberControl.php';
+
+// ALTER TABLE  `tb_dice_rolled` ADD INDEX (  `id` )
+
+session_name('tdRpg');
+session_set_cookie_params(2*7*24*60*60);
+session_start();
+
+if (isset($_SESSION['id'])) {
+    echo ':)';
+} else {
+    echo ':(';
+}
+
+echo '<br />';
+
+function _main() {  
+    if (isset($_GET['createboard'])) {
+        if (isset($_SESSION['id'])) {
+            $board = createBoard();
+
+            header('Location:?playing=true&id='.$board->getId().'&host_user='.$board->getHostUser().'&name='.$board->getName());
+        } else {
+            echo 'usuario não logado, favor <a href="'.baseURL().'?createacc=true">criar uma conta</a> ou <a href="'.baseURL().'?login=true">logar</a>!';
+        }
+    } else if (isset($_GET['playing'])) {
+        echo 'Id da mesa: '.$_GET['id'].'<br />Host da mesa: '.$_GET['host_user'];
+        
+        include 'pages/board.php';
+    } else if (isset($_GET['seeboard'])) {      
+        include 'pages/seeboard.php';
+    } else if (isset($_GET['createacc'])) {
+        include 'pages/createacc.php';
+    } else if (isset($_GET['login'])) {
+        include 'pages/login.php';
+    } else if (isset($_GET['logout'])) {
+        $_SESSION = array();
+        session_destroy();
+        
+        header('Location: http://'.$_SERVER['SERVER_NAME'].'/tdrpg/');
+        exit;
+    } else {
+        include 'pages/menu.php';
+    }
+}
+?>
